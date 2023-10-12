750ad34b15a06fbb5c0b548e5e8a71131184fcd1
DuongGiaHuy2007288 <dgiahuyct17tc31@gmail.com>
2023-10-12 21:58:09 +0700

    Thuc hanh

--- Lab_4/AWESOMEPROJECT/App.js
+++ Lab_4/AWESOMEPROJECT/App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ScreenA1 from './1a';
+import ScreenA2 from './1b';
+import ScreenA3 from './1c';
+import ScreenA4 from './1d';
+import ScreenA5 from './1e';
+import ScreenA6 from './2a';
+import ScreenA7 from './2c';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View> 
+      <ScreenA7 />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+      flex: 1,
+    },
+  });
+  
+
